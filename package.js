6e1b19a97b6be920207ed29f24b0a67a9c9b423a
Niskarsh-Rockstar <74487123+Niskarsh-Rockstar@users.noreply.github.com>
2021-04-02 14:54:33 +0530

    Add files via upload

---
+++ package.js
@@ -0,0 +1,34 @@
+// class package
+// {
+//     constructor (x,y)
+//     {
+//         var options=
+//         {
+//     isStatic : true,   
+//     restitution:0.8
+
+// }
+
+// this.body=Bodies.rectangle(x,y,width,height,options);
+// this.width=width;
+// this.height=height;
+// World.add(myWorld,this.body);
+// }
+// display()
+// {
+//     var pos=this.body.position;
+//     pos.x=mouseX
+//     pos.y=mouseY
+//     var angle=this.body.angle
+//     push();
+// translate(pos.x,pos.y);
+// rotate(angle);
+// strokeWeight(4);
+// stroke("red")
+// fill("yellow")
+// rectMode(CENTER)
+// rect(0,0,this.width,this.height)
+// pop()
+
+// }
+// }
